4c1f9b8d6d33b330c29b3f51b5d4a16ecac397b3
maranata2k26-netizen <maranata2k26@gmail.com>
2026-09-25 01:04:09 -0300

    feat: define four VST3 insert slots per input channel

--- canonical-current/app/MainComponent.h
+++ canonical-current/app/MainComponent.h
@@ -7,6 +7,7 @@
 #include "j3/ClickGenerator.h"
 #include "j3/Recording.h"
 #include "j3/Setlist.h"
+#include "j3/PluginCatalog.h"
 
 #include <array>
 #include <atomic>
@@ -31,6 +32,7 @@ private:
     static constexpr int kBuses = 8;
     static constexpr int kDcas = 8;
     static constexpr int kIemMixes = 16;
+    static constexpr int kPluginSlots = 4;
 
     class MixerStrip final : public juce::Component
     {
@@ -108,7 +110,7 @@ private:
     juce::File getAppStateFile() const;
     juce::File getRuntimeLockFile() const;
     void loadAppState();
-    void saveAppState();
+    void saveAppState(bool capturePluginState = false);
     void updateRecordingUi();
     void openDriverControlPanel();
     void showAudioError(const juce::String&);
@@ -126,6 +128,14 @@ private:
     void addSetlistSong();
     void removeSetlistSong();
     void loadSelectedSong();
+    void scanVst3Plugins();
+    void refreshPluginUi();
+    void loadSelectedPlugin();
+    void loadPluginPathIntoSlot(const juce::String& path, int channel, int slot);
+    void removeSelectedPlugin();
+    void openSelectedPluginEditor();
+    void restoreSavedPluginsAfterScan();
+    const float* processPluginChain(int channel, const float* input, int numSamples) noexcept;
     bool routeIsSafe(int paLeft, int paRight, int clickOutput) const noexcept;
     bool iemRouteIsSafe(int mix, int left, int right) const noexcept;
     void scheduleReconnect();
@@ -194,6 +204,17 @@ private:
     std::array<std::atomic<float>, kDcas> dcaGain_{};
     std::array<std::atomic<bool>, kDcas> dcaMute_{};
     juce::AudioBuffer<float> busScratch_;
+
+    j3::PluginCatalog pluginCatalog_;
+    juce::AudioPluginFormatManager pluginFormatManager_;
+    std::array<std::array<std::atomic<std::shared_ptr<juce::AudioPluginInstance>>, kPluginSlots>, kMaxChannels> channelPlugins_{};
+    std::array<std::array<std::atomic<bool>, kPluginSlots>, kMaxChannels> pluginBypass_{};
+    std::array<std::array<juce::String, kPluginSlots>, kMaxChannels> pluginPaths_{};
+    std::array<std::array<juce::String, kPluginSlots>, kMaxChannels> pluginNames_{};
+    std::array<std::array<juce::String, kPluginSlots>, kMaxChannels> pluginStateBase64_{};
+    juce::AudioBuffer<float> pluginScratch_;
+    juce::MidiBuffer pluginMidiScratch_;
+    bool pluginsScanned_ { false };
 
     std::array<std::array<std::atomic<float>, kMaxChannels>, kIemMixes> iemSendGain_{};
     std::array<std::array<std::atomic<float>, kMaxChannels>, kIemMixes> iemSendPan_{};
@@ -287,6 +308,18 @@ private:
     int selectedIemMix_ { 0 };
     int iemBankStart_ { 0 };
     std::array<std::unique_ptr<IemSendStrip>, kVisibleChannels> iemStrips_;
+
+    juce::Component pluginsPage_;
+    juce::Label pluginsTitle_;
+    juce::ComboBox pluginChannelBox_;
+    juce::ComboBox pluginSlotBox_;
+    juce::ComboBox pluginCatalogBox_;
+    juce::TextButton scanPluginsButton_ { "SCAN VST3" };
+    juce::TextButton loadPluginButton_ { "LOAD INSERT" };
+    juce::TextButton removePluginButton_ { "REMOVE" };
+    juce::ToggleButton bypassPluginButton_ { "BYPASS" };
+    juce::TextButton openPluginEditorButton_ { "OPEN PARAMETERS" };
+    juce::Label pluginStatusLabel_;
 
     juce::Component padPage_;
     juce::Label padTitle_;
