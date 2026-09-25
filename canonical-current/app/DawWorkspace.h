9f2907f0dd2107ec4dd315cce921ddc5466268a5
maranata2k26-netizen <maranata2k26@gmail.com>
2026-09-25 03:30:22 -0300

    Fix DAW workspace header line break

--- canonical-current/app/DawWorkspace.h
+++ canonical-current/app/DawWorkspace.h
@@ -211,7 +211,8 @@ private:
 
     juce::File projectFile_;
     bool projectDirty_ { false };
-    int autosaveTicks_ { 0 };\n    bool lastReportedPlaying_ { false };
+    int autosaveTicks_ { 0 };
+    bool lastReportedPlaying_ { false };
 
     juce::TextButton newButton_ { "NUEVO" };
     juce::TextButton openButton_ { "ABRIR" };
