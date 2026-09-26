d323446afb3e68990ee52e5c7da13a5aaab3a8b3
maranata2k26-netizen <maranata2k26@gmail.com>
2026-09-26 11:52:20 -0300

    Update updater client version

--- canonical-current/app/UpdateService.cpp
+++ canonical-current/app/UpdateService.cpp
@@ -81,7 +81,7 @@ bool UpdateService::requestBytes(const juce::String& url, juce::MemoryBlock& byt
     if (object.empty())
         object = L"/";
 
-    InternetHandle session { WinHttpOpen(L"J3Worship/1.6",
+    InternetHandle session { WinHttpOpen(L"J3Worship/1.7",
         WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME,
         WINHTTP_NO_PROXY_BYPASS, 0) };
     if (session.handle == nullptr)
